daae8a544da0d6f2d215b8551a6981fce521b9e1
ovidiucs <ovidiu.ctin@gmail.com>
2015-12-22 20:22:07 +0200

    intial

---
+++ urblog.sql
@@ -0,0 +1,8 @@
+CREATE TABLE uw_Urblog_entry(uw_id int8 NOT NULL, uw_title text NOT NULL, 
+                              uw_created timestamp NOT NULL, 
+                              uw_author text NOT NULL, uw_body text NOT NULL,
+ PRIMARY KEY (uw_id)
+  
+ );
+ 
+ 
